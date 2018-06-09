ceb8bf7057f4c3157e94921e772fcf06df94a471
Matt Lantz <mattlantz@gmail.com>
2018-06-09 16:52:46 -0400

    Starting Atlas

---
+++ src/Atlas.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Grafite\MissionControl;
+
+use Unirest\Request as UniRequest;
+
+class Atlas
+{
+    public $token;
+
+    protected $missionControlUrl;
+
+    public function __construct($token = null)
+    {
+        if (!is_null($token)) {
+            $this->token = $token;
+        }
+
+        $this->missionControlUrl = 'http://missioncontrol.test/api/atlas';
+    }
+
+    /**
+     * Send the exception to Mission control.
+     *
+     * @param Exeption $exception
+     *
+     * @return bool
+     */
+    public function analyzePerformance()
+    {
+        $headers = [
+            'token' => $this->token,
+        ];
+
+        $query = $this->getTraffic();
+
+        $response = UniRequest::post($this->missionControlUrl, $headers, $query);
+
+        if ($response->code != 200) {
+            error_log('Unable to message Grafite Mission Control, please confirm your token');
+        }
+    }
+
+    /**
+     * Collect data and set report details.
+     *
+     * @return array
+     */
+    public function getTraffic()
+    {
+        //
+
+
+
+        return [
+            'memory' => 9,
+            'storage' => 99,
+            'cpu' => 0.3,
+        ];
+    }
+}
