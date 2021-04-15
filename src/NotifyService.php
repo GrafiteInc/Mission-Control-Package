79fae3f39e6181cb8adedf4c1ec650022a20de4f
Matt Lantz <mattlantz@gmail.com>
2021-04-15 18:15:53 -0400

    because this should be the key not the token

--- src/NotifyService.php
+++ src/NotifyService.php
@@ -35,7 +35,7 @@ class NotifyService extends BaseService
     {
         $headers = [
             'Authorization' => 'Bearer ' . $this->token,
-            'token' => $this->token,
+            'token' => $this->key,
         ];
 
         if (is_null($this->token)) {
